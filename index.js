621bcf9393bd38b865639c1435da7275dd6f3bbe
Wen Xiao <wennstyle@gmail.com>
2021-10-05 23:12:10 -0400

    commit tree e489a4c0eed5f7c57e3a7dc152567edcedc2e7d7

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+//no longer has DOM node 'main#main'
+const removeMain = document.getElementById("main")
+removeMain.remove()
+
+//has a 'newHeader' variable that points to node 'h1#victory'
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+//has has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.innerHTML = "Wen is the champion"
+
+document.body.append(newHeader)
